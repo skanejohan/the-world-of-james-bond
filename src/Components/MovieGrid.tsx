0c53b3ffa593cf2d6c8d068b0a1106b770177704
skanejohan <johan_m_ahlgren@hotmail.com>
2025-02-02 15:34:40 +0100

    Adjust grid dimensions

--- src/Components/MovieGrid.tsx
+++ src/Components/MovieGrid.tsx
@@ -7,9 +7,16 @@ interface Props {
 }
 
 const MovieGrid = ({movies, onSelect}: Props) => {
+  const _def = "    grid-cols-1     gap-6      px-4";
+  const __sm = " sm:grid-cols-1  sm:gap-6   sm:px-4";
+  const __md = " md:grid-cols-2  md:gap-6   md:px-8";
+  const __lg = " lg:grid-cols-3  lg:gap-8   lg:px-16";
+  const __xl = " xl:grid-cols-4  xl:gap-10  xl:px-32";
+  const _2xl = "2xl:grid-cols-5 2xl:gap-12 2xl:px-64";
+  const cls = `container m-auto py-12 grid ${_def} ${__sm} ${__md} ${__lg} ${__xl} ${_2xl}`;
   return (
     <>
-        <div className="container m-auto py-12 lg:px-60 grid gap-12 grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+        <div className={cls}>
             {movies.map(m => <MovieTile key={m.id} movie={m} onSelect={onSelect}/>)}
         </div>
     </>
