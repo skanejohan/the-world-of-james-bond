33d147e651a1c74428c65421ca6339f16f4c1b02
skanejohan <johan_m_ahlgren@hotmail.com>
2025-02-11 19:20:47 +0100

    Click handler, animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import MovieGrid from './Components/MovieGrid';
 import { Movies } from './Data/movies';
 import './Styles/styles.css';
+import { IMovie } from './types';
+import { Fade } from "react-awesome-reveal";
 
 function App() {
+  const [selectedMovie, setSelectedMovie] = useState<IMovie | null>();
+
   return (
-    <>
-      <MovieGrid movies={Movies} onSelect={movie => console.log(movie)} />
-    </>
+    selectedMovie == null
+      ? <div>
+          <Fade duration={1000}>
+            <MovieGrid movies={Movies} onSelect={setSelectedMovie} />
+          </Fade>
+        </div>
+      : <div>
+          <div>
+            <Fade duration={1000}>
+              <img src={selectedMovie.poster} className="rounded-sm" onClick={() => setSelectedMovie(null)}/>
+            </Fade>
+          </div>
+        </div>
   )
 }
 
--- src/Components/MovieTile.tsx
+++ src/Components/MovieTile.tsx
@@ -7,16 +7,23 @@ interface Props {
 
 const MovieTile = ({ movie, onSelect }: Props) => {
 
-    const divClass = movie.enabled ? "" : "grayscale";
+    const divBaseClass = `bg-gray-800 `;
+    const divClass = movie.enabled ? divBaseClass : `${divBaseClass} grayscale`;
 
     const buttonBaseClass = "rounded-md border-4";
     const buttonClass = movie.enabled 
         ? `${buttonBaseClass} border-sky-500 cursor-pointer transition duration-300 hover:scale-105` 
         : `${buttonBaseClass} border-sky-800 cursor-default`;
 
+    const clickHandler = () => {
+        if (movie.enabled) {
+            onSelect(movie);
+        }
+    }
+
     return (
         <div className={divClass}>
-            <button className={buttonClass} onClick={() => onSelect(movie)}>
+            <button className={buttonClass} onClick={clickHandler}>
                 <img src={movie.poster} className="rounded-sm" />
             </button>
         </div>
